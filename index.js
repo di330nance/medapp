7d9251a77c7c2916b8fe96c3be7c0661d400b5c1
di330nance <di330nance@gmail.com>
2024-11-04 18:54:53 +0300

    Add styles

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
+
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+
+app.post('/register', (req, res) => {
+    const { login, password, userType } = req.body;
+
+
+    const dataToSave = `Login: ${login}, Password: ${password}, UserType: ${userType}\n`;
+
+    /* Записываем данные в файл */
+    fs.appendFile('registrations.txt', dataToSave, (err) => {
+        if (err) {
+            console.error('Ошибка записи в файл', err);
+            return res.status(500).send('Ошибка сервера');
+        }
+        res.send(`Registered with login: ${login}`);
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
